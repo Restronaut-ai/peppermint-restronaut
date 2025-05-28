ce092e381826c99b96eb0e5682faa9dd494b1350
Pukar Sharma <sharmapukar217@gmail.com>
2025-05-28 17:35:11 +0000

    FIX: Branding...

--- apps/client/pages/auth/forgot-password.tsx
+++ apps/client/pages/auth/forgot-password.tsx
@@ -37,11 +37,11 @@ export default function Login({}) {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <a target="_blank" href="https://peppermint.sh/">
+        <a target="_blank" href="https://restronaut.ai/">
           <img
             className="mx-auto h-36 w-auto"
             src="/login.svg"
-            alt="peppermint.sh logo"
+            alt="Restronaut.ai logo"
           />
         </a>
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
